56b91307e277c752ea9973b8d95563f9d5139766
volatile rye <volatilerye@gmail.com>
2025-06-30 03:05:24 +0900

    fix: changed script and .gitignore

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -3,3 +3,5 @@
 python -X utf8 ../../scripts/replace_book_to_site.py
 python -X utf8 ../../scripts/replace_alert.py
 python -X utf8 ../../scripts/replace_nav_chapters.py
+mv ../html/* ..
+rm -rf ../html ../replace
