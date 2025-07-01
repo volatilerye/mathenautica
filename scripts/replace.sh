9afb7dd94f66ecf0098f21315f165cc10e54b896
volatile rye <volatilerye@gmail.com>
2025-07-01 23:29:25 +0900

    test: fixed book.toml

--- scripts/replace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# python -X utf8 ../../scripts/replace_book_to_site.py
-# python -X utf8 ../../scripts/replace_alert.py
-# python -X utf8 ../../scripts/replace_nav_chapters.py
-# rm -rf replace
