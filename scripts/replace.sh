64f4e9259d6c4a459f6cc476c351cc6245b1e631
volatile rye <volatilerye@gmail.com>
2025-07-01 22:48:02 +0900

    fix: changed toml, etc.

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -3,5 +3,4 @@
 python -X utf8 ../../scripts/replace_book_to_site.py
 python -X utf8 ../../scripts/replace_alert.py
 python -X utf8 ../../scripts/replace_nav_chapters.py
-mv ../html/* ..
-rm -rf ../html ../replace
+rm -rf replace
