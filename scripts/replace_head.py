a57ad57c156a99268cdf27bf7db59a9b5f2404af
volatile rye <volatilerye@gmail.com>
2025-07-04 22:37:10 +0900

    fix: add missing [build] section in book.toml and update replace_head.py for dynamic Open Graph title

--- scripts/replace_head.py
+++ scripts/replace_head.py
@@ -9,19 +9,17 @@ def replace_ogp(html: Path) -> None:
     with html.open(encoding="utf-8") as f:
         replaced = f.read()
         matched = re.search(r'<title>(.*) \- (.*)</title>', replaced)
-        print(html)
         if not matched:
             return  # No match found, skip this file
-        print(matched.group(1), matched.group(2))
 
         replaced = re.sub(
             r'<meta content="" property="og:title"/>',
-            f'<meta content="MATHENAUTICA" property="og:title"/>',
+            f'<meta content="{repr(matched.group(1))[1:-1]}" property="og:title"/>',
             replaced
         )
         replaced = re.sub(
             r'<meta content="" property="og:site_name"/>',
-            f'<meta content="{matched.group(2)}" property="og:site_name"/>',
+            f'<meta content="MATHENAUTICA" property="og:site_name"/>',
             replaced
         )
         replaced = re.sub(
--- scripts/replace_title.py
+++ scripts/replace_title.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from typing import Final
 
 BASE_HTML_DIR: Final[Path] = Path("../html")
-
+EXCLUDE_INVERT_E: Final[set[str]] = {
+    '<meta content="MATHENAUTICA" property="og:site_name"/>',
+    r'^<title>(.+) \- MATHENAUTICA</title>$'
+}
 
 def replace_book_to_site(html: Path) -> None:
     with html.open(encoding="utf-8") as f:
@@ -18,11 +21,14 @@ def replace_book_to_site(html: Path) -> None:
 
 def invert_e(html: Path) -> None:
     with html.open(encoding="utf-8") as f:
-        replaced = re.sub(
-            r"MATHENAUTICA(?!\</title\>)(.*)",
-            r'MATH<div class="invert-e">E</div>NAUTICA\1',
-            f.read(),
-        )
+        replaced = f.read()
+        matched = re.finditer(r"^(.*)(MATHENAUTICA)(.*)$", replaced, flags = re.MULTILINE)
+        for match in list(matched)[::-1]:
+            for exclude in EXCLUDE_INVERT_E:
+                if re.match(exclude, match.group(0)):
+                    break
+            else:
+                replaced = replaced[:match.start(2)] + "MATH<div class='invert-e'>E</div>NAUTICA" + replaced[match.end(2):]
 
     with html.open("w", encoding="utf-8") as f:
         f.write(replaced)
